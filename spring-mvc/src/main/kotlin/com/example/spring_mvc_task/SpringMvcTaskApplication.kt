fb5ae2c455d74d3191d848cd4f82fd7035494bf3
Vincent Marillion <vmarilli@student.21-school.ru>
2021-11-01 05:33:32 +0300

    mvc task

---
+++ spring-mvc/src/main/kotlin/com/example/spring_mvc_task/SpringMvcTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_mvc_task
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMvcTaskApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringMvcTaskApplication>(*args)
+}
